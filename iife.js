f6e50c86f75f546c669b90142e2f95dbc089e8ac
Kiran Adhikari <kiranadh1452@gmail.com>
2022-09-29 10:14:05 +0545

    IIFE

---
+++ iife.js
@@ -0,0 +1,13 @@
+/**
+ * description
+ */
+(function printHello() {
+    console.log("Hello");
+})();
+
+/**
+ * description: Use of arrow function
+ */
+(() => {
+    console.log("My name");
+})();
